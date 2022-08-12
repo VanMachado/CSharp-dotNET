eb3dc640f1ada420e4df58b011d012f94a61bc1a
VanMachado <vandamme.machado@gmail.com>
2022-08-12 20:02:22 -0300

    Explorando conceitos basicos de encapsulamento

---
+++ ExercicioFixacao/ExercicioFixacao/Conta.cs
@@ -0,0 +1,34 @@
+﻿namespace ExercicioFixacao {
+    internal class Conta {
+        private int NumeroConta { get; }
+        public string Nome { get; private set; }
+        public double Saldo { get; private set; }
+
+        public Conta(int num, string nome, double deposito) {
+            this.NumeroConta = num;
+            this.Nome = nome;
+            this.Saldo = deposito;
+        }
+                        
+        public void setNome(string nome2) {
+            if(nome2 != this.Nome) {
+                this.Nome = nome2;
+            }
+        }
+
+        public void Deposita(double deposito) {
+            this.Saldo += deposito;
+        }
+
+        public void Saca(double saque) {
+            this.Saldo -= saque + 5.00;
+        }
+
+        public override string ToString() {            
+            return ("Conta " + this.NumeroConta +
+                ", Titular: " + this.Nome +
+                ", Saldo: R$" + this.Saldo.ToString("F2"));
+        }
+
+    }
+}
---
+++ ExercicioFixacao/ExercicioFixacao/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace ExercicioFixacao {
+    class Program {
+        static void Main(string[] args) {
+            string decisao;
+            double depoInicial;
+            double depo;
+            double saque;
+
+            Console.WriteLine("Entre o número da conta: ");
+            int num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com o titular da conta: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Deseja fazer um depósito inicial? (s/n): ");
+            decisao = Console.ReadLine();
+
+            if (decisao == "s") {
+                Console.WriteLine("Entre com o valor do depósito inicial: ");
+                depoInicial = double.Parse(Console.ReadLine());
+                Console.WriteLine();
+            } else {
+                depoInicial = 0.00;
+            }                       
+            
+            Conta conta = new Conta(num, nome, depoInicial);
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(conta);
+            Console.WriteLine();
+
+            Console.WriteLine("Entre com um valor para deposito: ");
+            depo = double.Parse(Console.ReadLine());
+            conta.Deposita(depo);
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta atualizado: ");
+            Console.WriteLine(conta);
+            Console.WriteLine();
+
+            Console.WriteLine("Entre com um valor para saque: ");
+            saque = double.Parse(Console.ReadLine());
+            conta.Saca(saque);
+            Console.WriteLine("Dados da conta atualizado: ");
+            Console.WriteLine(conta);                       
+        }
+    }
+}
