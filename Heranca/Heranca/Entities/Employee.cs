df6f1ea9e59f7373a0e08849fc93aefc7b4570b0
VanMachado <vandamme.machado@gmail.com>
2022-08-22 14:16:07 -0300

    Explorando conceitos básicos de herança

---
+++ Heranca/Heranca/Entities/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+using System.Text;
+
+namespace Heranca.Entities
+{
+    internal class Employee
+    {
+        public string Name { get; private set; }
+        public int Hours { get; private set; }
+        public double ValuePerHour { get; private set; }
+
+        public Employee()
+        {
+        }
+
+        public Employee(string name, int hours, double valuePerHour)
+        {
+            Name = name;
+            Hours = hours;
+            ValuePerHour = valuePerHour;
+        }
+
+        public virtual double Payment()
+        {
+            return ValuePerHour * Hours;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();            
+            sb.Append(Name + " - $ " + Payment().ToString("F2", CultureInfo.InvariantCulture));
+
+            return sb.ToString();  
+        }
+    }
+}
---
+++ Heranca/Heranca/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Globalization;
+using Heranca.Entities;
+
+namespace Heranca
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Employee> lista = new List<Employee>();
+
+            Console.Write("Type how many employee will be add: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Employee #{i+1} data:");
+                Console.WriteLine("Outsourcede (y/n)? ");
+                char resp = char.Parse(Console.ReadLine());
+                if(resp == 'y' || resp == 'Y')
+                {
+                    Console.Write("Name: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Hours: ");
+                    int hours = int.Parse(Console.ReadLine());
+                    Console.Write("Value per hour: ");
+                    double value = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    Console.Write("Charge: ");
+                    double charge = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                    OutsourcedEmployee outEmp = new OutsourcedEmployee
+                        (name, hours, value, charge);
+                    lista.Add(outEmp);
+                } else
+                {
+                    Console.Write("Name: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Hours: ");
+                    int hours = int.Parse(Console.ReadLine());
+                    Console.Write("Value per hour: ");
+                    double value = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                    Employee emp = new Employee(name, hours, value);
+                    lista.Add(emp);
+                }
+            }
+            
+            Console.WriteLine();
+            Console.WriteLine("PAYMENTS");
+
+            foreach(Employee emp in lista)
+            {
+                Console.WriteLine(emp);
+            }
+        }
+    }
+}
